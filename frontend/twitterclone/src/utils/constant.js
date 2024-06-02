b043a0ad29a96136b3f4c0d678d0157685971c8c
Yogita Khanna <yogitakhanna06@gmail.com>
2024-06-03 01:13:25 +0530

    first

--- frontend/twitterclone/src/utils/constant.js
+++ frontend/twitterclone/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
-export const TWEET_API_END_POINT = "http://localhost:8080/api/v1/tweet";
+export const USER_API_END_POINT = "https://twitter-two-ruby.vercel.app/api/v1/user";
+export const TWEET_API_END_POINT = "https://twitter-two-ruby.vercel.app/api/v1/tweet";
 
 export const timeSince = (timestamp) => {
     let time = Date.parse(timestamp);
