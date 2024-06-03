381b619c2b1c70263a025280517ab8deac6147d2
Yogita Khanna <yogitakhanna06@gmail.com>
2024-06-03 22:41:46 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin:"http://localhost:3000",
+    origin:"https://twitter-o7tn.vercel.app",
     credentials:true
 }
 
